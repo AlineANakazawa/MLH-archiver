4933eacc4e06a91c202fec8f7746c35e11582825
Rafael Passos <rafael@rcpassos.me>
2026-05-25 16:35:39 -0300

    feat: split example list data in two
    Signed-off-by: Rafael Passos <rafael@rcpassos.me>

--- scripts/create_example_pi/create_example_public_inbox.pl
+++ scripts/create_example_pi/create_example_public_inbox.pl
@@ -56,32 +56,47 @@ foreach my $file (sort @eml_files) {
 
 say "Parsed " . scalar(@emails) . " emails from " . scalar(@eml_files) . " .eml files";
 
-my $inbox_name = "example";
-my $inbox_dir = File::Spec->catdir($out_dir, "v2_$inbox_name");
+# change this value if you want a diferent number of lists
 
-say "Creating V2 inbox: $inbox_name";
+my $number_of_lists = 2;
+my $total_emails = scalar @emails;
+my $per_list = int(($total_emails + $number_of_lists - 1) / $number_of_lists);
 
-PublicInbox::TestCommon::create_inbox(
-    $inbox_name,
-    version => 2,
-    tmpdir => $inbox_dir,
-    sub {
-        my ($importer, $ibx) = @_;
-        foreach my $eml (@emails) {
-            $importer->add($eml);
+for my $i (0 .. $number_of_lists - 1) {
+    my $start = $i * $per_list;
+    my $end = $start + $per_list;
+    $end = $total_emails if $end > $total_emails;
+
+    my @chunk = @emails[$start .. $end - 1];
+    next unless @chunk;
+
+    my $inbox_name = "list_$i";
+    my $inbox_dir = File::Spec->catdir($out_dir, "v2_$inbox_name");
+
+    say "Creating V2 inbox $inbox_name (" . scalar(@chunk) . " emails)";
+
+    PublicInbox::TestCommon::create_inbox(
+        $inbox_name,
+        version => 2,
+        tmpdir => $inbox_dir,
+        sub {
+            my ($importer, $ibx) = @_;
+            foreach my $eml (@chunk) {
+                $importer->add($eml);
+            }
         }
-    }
-);
+    );
 
-say "Inbox created at: $inbox_dir";
+    say "  Inbox created at: $inbox_dir";
+
+    unlink glob("$inbox_dir/creat.*");
+    unlink "$inbox_dir/inbox.lock";
+    unlink "$inbox_dir/open.lock";
+    unlink "$inbox_dir/msgmap.sqlite3-journal";
+}
 
 my $t_dir = File::Spec->catdir($out_dir, 't');
 if (-d $t_dir) {
     File::Path::remove_tree($t_dir);
     say "Cleaned up temp directory: $t_dir";
 }
-
-unlink glob("$inbox_dir/creat.*");
-unlink "$inbox_dir/inbox.lock";
-unlink "$inbox_dir/open.lock";
-unlink "$inbox_dir/msgmap.sqlite3-journal";
